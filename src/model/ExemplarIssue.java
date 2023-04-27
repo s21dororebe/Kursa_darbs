4d676a1f1b949675beafc7baddb878921b442b00
s21dororebe <s21dororebe@venta.lv>
2023-04-27 19:42:12 +0300

    Edit: ExemplarIssue class done (TODO lists)

--- src/model/ExemplarIssue.java
+++ src/model/ExemplarIssue.java
@@ -12,19 +12,15 @@ public class ExemplarIssue {
     public User getUser() {
         return user;
     }
-
     public Librarian getLibrarian() {
         return librarian;
     }
-
     public Exemplar getExemplar() {
         return exemplar;
     }
-
     public LocalDateTime getDateTimeBookIsIssued() {
         return dateTimeBookIsIssued;
     }
-
     public LocalDateTime getDateBookReturn() {
         return dateTimeBookReturn;
     }
@@ -44,7 +40,9 @@ public class ExemplarIssue {
     public void setExemplar(Exemplar exemplar) throws Exception {
         if(exemplar != null){
             //TODO find exemplar in exemplar list
-            this.exemplar = exemplar;
+            if(!exemplar.isIssued()){
+                this.exemplar = exemplar;
+            } else throw new Exception("This exemplar is taken");
         } else throw new Exception("Invalid input exemplar");
     }
 
@@ -53,12 +51,35 @@ public class ExemplarIssue {
             this.dateTimeBookIsIssued = dateTimeBookIsIssued;
         } else throw new Exception("Invalid input date and time when book was issued");
     }
-
-   /* public void setDateTimeBookReturn(LocalDateTime dateTimeBookReturn) throws Exception {
+    public void setDateTimeBookReturn(LocalDateTime dateTimeBookReturn) throws Exception {
         if(dateTimeBookReturn != null){
-            if(dateTimeBookReturn >= LocalDateTime.now()){
+            if(dateTimeBookReturn.isAfter(dateTimeBookIsIssued)){
                 this.dateTimeBookReturn = dateTimeBookReturn;
             }
         } else throw new Exception("Invalid input date object");
-    }*/
+    }
+
+    public ExemplarIssue(User user, Librarian librarian, Exemplar exemplar, LocalDateTime dateTimeBookIsIssued, LocalDateTime dateTimeBookReturn) throws Exception {
+        setUser(user);
+        setLibrarian(librarian);
+        setExemplar(exemplar);
+        setDateTimeBookIsIssued(dateTimeBookIsIssued);
+        setDateTimeBookReturn(dateTimeBookReturn);
+    }
+    public ExemplarIssue(User user, Librarian librarian, Exemplar exemplar) throws Exception {
+        setUser(user);
+        setLibrarian(librarian);
+        setExemplar(exemplar);
+    }
+
+    @Override
+    public String toString() {
+        return "ExemplarIssue{" +
+                "user=" + user +
+                ", librarian=" + librarian +
+                ", exemplar=" + exemplar +
+                ", dateTimeBookIsIssued=" + dateTimeBookIsIssued +
+                ", dateTimeBookReturn=" + dateTimeBookReturn +
+                '}';
+    }
 }
