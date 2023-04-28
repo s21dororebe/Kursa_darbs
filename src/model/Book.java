7dc1a42f3e17153be434304c8a66dea248adf3b0
s21dororebe <s21dororebe@venta.lv>
2023-04-28 19:36:45 +0300

    Edit: stylized by Rebeka

--- src/model/Book.java
+++ src/model/Book.java
@@ -1,7 +1,6 @@
 package model;
 
 import java.time.LocalDateTime;
-
 import Enum.BookGenre;
 
 public class Book {
@@ -12,70 +11,62 @@ public class Book {
 	private BookGenre genre;
 	private String description;
 	private int quantity = 0;
-	private LocalDateTime publicationdate;
-	
+	private LocalDateTime publicationDate;
 	
 	// Constructors
-	
-	
-	public Book(String title, Author author, BookGenre genre, String description, int quantity, LocalDateTime publicationdate) throws Exception {
+	public Book(String title, Author author, BookGenre genre, String description, int quantity, LocalDateTime publicationDate) throws Exception {
+        setGeneratedId();
 		setTitle(title);
 		setAuthor(author);
 		setGenre(genre);
 		setDescription(description);
 		setQuantity(quantity);
-		setPublicationDate(publicationdate);
+		setPublicationDate(publicationDate);
     }
-	
-	
-	
 	// Setters and Getters
 	
 	public long getGeneratedId() {
 		return generatedId;
 	}
-
 	public void setGeneratedId() {
 		this.generatedId = idCounter++;
 	}
-        
-        
+
 	public String getTitle() {
         return title;
     }
     public void setTitle(String title) throws Exception {
-    	if (title == null) {
-            throw new Exception("Title cannot be empty");
-        }
-        this.title = title;
+        if(title.length() > 0 && title.length() < 100){
+            this.title = title;
+        } else throw new Exception("Title cannot be empty");
     }
     
     public Author getAuthor() {
     	return author;
     }
-    public void setAuthor(Author author) {
-    	this.author = author;
+    public void setAuthor(Author author) throws Exception {
+        if(author != null)
+            this.author = author;
+        else throw new Exception("Invalid input author");
     }
-    
     
     public BookGenre getGenre() {
         return genre;
     }
-    public void setGenre(BookGenre genre) {
-        this.genre = genre;
+    public void setGenre(BookGenre genre) throws Exception {
+        if(genre != null)
+            this.genre = genre;
+        else throw new Exception("Invalid input genre");
     }
-    
-    
+
     public String getDescription() {
         return description;
     }
     public void setDescription(String description) throws Exception {
-    	if (description == null) {
-            throw new Exception("Description cannot be empty");
-        }
-        this.description = description;
+        if(description.length() > 0 && description.length() < 1000){
+            this.description = description;
+        } else throw new Exception("Description cannot be empty");
     }
-    
     
     public int getQuantity() {
     	return quantity;
@@ -84,22 +75,20 @@ public class Book {
     	if (quantity <= 99 && quantity > 0) {
             this.quantity = quantity;
         } else {
-            throw new Exception("Too much books");
+            throw new Exception("Invalid input quantity (it can be from 1 to 99)");
         }
     }
-    
-    
+
     public LocalDateTime getPublicationDate() {
-        return publicationdate;
+        return publicationDate;
     }
-    public void setPublicationDate(LocalDateTime publicationdate) throws Exception {
-    	if (publicationdate.isAfter(LocalDateTime.now())) {
+    public void setPublicationDate(LocalDateTime publicationDate) throws Exception {
+    	if (publicationDate.isAfter(LocalDateTime.now())) {
             throw new Exception("Publication date cannot be in the future");
         }
-        this.publicationdate = publicationdate;
+        this.publicationDate = publicationDate;
     }
-    
-    
+
     @Override
     public String toString() {
         return "Book{" +
@@ -108,7 +97,7 @@ public class Book {
                 ", genre=" + genre +
                 ", description='" + description + '\'' +
                 ", quantity=" + quantity +
-                ", publicationdate=" + publicationdate +
+                ", publicationdate=" + publicationDate +
                 '}';
     }
     
